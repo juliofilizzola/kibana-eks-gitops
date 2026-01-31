25ac4456a7a29e56856343860b8db9d88089ad66
julio filizzola <juliofilizzola@hotmail.com>
2026-01-31 16:55:16 -0300

    Update Terraform configuration: refine AWS EKS setup, add VPC module, configure namespaces, and enhance Helm provider usage

--- infra/main.tf
+++ infra/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = var.region
 }
 
+# ================= VPC =================
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
@@ -9,97 +10,123 @@ module "vpc" {
   name = "${var.cluster_name}-vpc"
   cidr = var.vpc_cidr
 
-  azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs              = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  private_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets   = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
 
   public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
+    "kubernetes.io/role/elb"                = 1
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
   }
   private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/role/internal-elb"       = 1
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
   }
 }
 
+module "aws_auth" {
+  source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
+  version = "~> 20.24"
+
+  manage_aws_auth_configmap = true
+
+  aws_auth_roles = [
+    {
+      rolearn  = "arn:aws:iam::052775460750:role/EKSAdminRole"
+      username = "eks-admin"
+      groups   = ["system:masters"]
+    }
+  ]
+
+  depends_on = [module.eks]
+}
+
+
+# ================= EKS CLUSTER =================
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "~> 20.24"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.30"
+  cluster_version = "1.35"
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access       = true
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
   eks_managed_node_groups = {
     general = {
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-
-      instance_types = ["t3.medium"]
+      name             = "general"
+      use_name_prefix  = false
+      min_size         = 1
+      max_size         = 3
+      desired_size     = 2
+      instance_types   = ["t3.medium"]
     }
   }
-
-  # Argo CD IAM para acessar EKS
-  enable_irsa = true
 }
 
-# Configurar providers Kubernetes/Helm para usar o cluster criado
+
+# Data sources (dependem do EKS)
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name       = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name       = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
+# ================= PROVIDERS K8S/HELM CORRIGIDOS =================
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
 }
 
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   }
 }
 
-# Namespace Elastic
+# Namespaces
 resource "kubernetes_namespace" "elastic" {
   metadata {
     name = "elastic-system"
   }
+  depends_on = [module.eks.eks_managed_node_groups]
 }
 
-# Namespace Argo CD
 resource "kubernetes_namespace" "argocd" {
   metadata {
     name = "argocd"
   }
+  depends_on = [module.eks.eks_managed_node_groups]
 }
 
-# Instalar Argo CD via Helm
+# Argo CD (versão estável)
 resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  namespace  = "argocd"
-  create_namespace = false
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+  version    = "6.6.5"
 
-  version = "6.6.5"  # Última estável
+  depends_on = [kubernetes_namespace.argocd, module.eks.eks_managed_node_groups]
 
-  values = [yamlencode({
-    server = {
-      service = {
-        type = "LoadBalancer"
-      }
-    }
-  })]
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
 }
-
